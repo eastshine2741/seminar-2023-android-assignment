079aea7d20381937d451660d2b7c5189488e480c
eastshine2741 <eastshine@snu.ac.kr>
2023-09-04 18:25:28 +0900

    일단 함

--- assignment-1/app/src/main/java/com/example/assignment1/CalculatorActivity.kt
+++ assignment-1/app/src/main/java/com/example/assignment1/CalculatorActivity.kt
@@ -1,22 +1,12 @@
 package com.example.assignment1
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.example.assignment1.databinding.ActivityCalculatorBinding
-import com.example.assignment1.viewmodel.MainViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.forEach
-import kotlinx.coroutines.launch
 
 class CalculatorActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCalculatorBinding
-    private val _operator = MutableStateFlow<String>("")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,33 +17,34 @@ class CalculatorActivity : AppCompatActivity() {
         val userPw = intent.getStringExtra("pw")
         binding.userId = userId
         binding.userPw = userPw
+        binding.operator = "+"
+        binding.result = "0"
 
         binding.addButton.setOnClickListener {
-            lifecycleScope.launch { _operator.emit("+") }
+            calculate("+")
         }
         binding.subtractButton.setOnClickListener {
-            lifecycleScope.launch { _operator.emit("-") }
+            calculate("-")
         }
         binding.multiplyButton.setOnClickListener {
-            lifecycleScope.launch { _operator.emit("*") }
+            calculate("*")
         }
         binding.divideButton.setOnClickListener {
-            lifecycleScope.launch { _operator.emit("/") }
+            calculate("/")
         }
-        lifecycleScope.launch {
-            _operator.collect {
-                if (binding.firstEdittext.text.isNotEmpty() && binding.secondEdittext.text.isNotEmpty()) {
-                    val first = binding.firstEdittext.text.toString().toFloat()
-                    val second = binding.secondEdittext.text.toString().toFloat()
-                    binding.resultText.text = when (it) {
-                        "+" -> (first+second).toString()
-                        "-" -> (first-second).toString()
-                        "*" -> (first*second).toString()
-                        else -> (first/second).toString()
-                    }
-                }
-                binding.operatorText.text = _operator.value
+    }
+
+    fun calculate(operator: String) {
+        if (binding.firstEdittext.text.isNotEmpty() && binding.secondEdittext.text.isNotEmpty()) {
+            val first = binding.firstEdittext.text.toString().toFloat()
+            val second = binding.secondEdittext.text.toString().toFloat()
+            binding.resultText.text = when (operator) {
+                "+" -> (first+second).toString()
+                "-" -> (first-second).toString()
+                "*" -> (first*second).toString()
+                else -> (first/second).toString()
             }
         }
+        binding.operator = operator
     }
 }
--- assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
+++ assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -4,14 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.viewModels
 import com.example.assignment1.databinding.ActivityMainBinding
-import com.example.assignment1.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         binding.loginButton.setOnClickListener {
             if (binding.pwEdittext.text.toString().length < 5) {
                 Toast.makeText(this, "유효하지 않은 비밀번호입니다", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
             val intent = Intent(this, CalculatorActivity::class.java)
             intent.putExtra("id", binding.idEdittext.text.toString())
