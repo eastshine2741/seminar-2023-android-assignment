c2721b69b7e053ab36c906965ec70dce1c9909a0
eastshine2741 <eastshine@snu.ac.kr>
2023-10-05 04:07:22 +0900

    gradle

--- assignment-3/app/build.gradle.kts
+++ assignment-3/app/build.gradle.kts
@@ -44,4 +44,17 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //retrofit
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+
+    //moshi
+    implementation("com.squareup.moshi:moshi:1.14.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+
+    //hilt
+    implementation("com.google.dagger:hilt-android:2.47")
+    implementation("com.google.dagger:hilt-android-compiler:2.47")
 }
---
+++ assignment-3/app/src/main/java/com/jutak/assignment3/MainApplication.kt
@@ -0,0 +1,12 @@
+package com.jutak.assignment3
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
